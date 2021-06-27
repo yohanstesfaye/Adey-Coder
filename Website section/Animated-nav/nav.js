abf37b8fa612b8d576ae37e0ec48af08ea72afc1
Yohans Tesfaye <45452580+yohans-t@users.noreply.github.com>
2021-06-27 17:43:45 -0300

    Animated navigation

---
+++ Website section/Animated-nav/nav.js
@@ -0,0 +1,10 @@
+menu = $("nav ul");
+toggle = $("nav ul .menu");
+
+toggle.on("click", function() {
+  if (menu.hasClass("active")) {
+    menu.removeClass("active");
+  } else {
+    menu.addClass("active");
+  }
+});
